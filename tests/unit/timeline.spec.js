570d5d57e9145620e079a19e07780a789db302be
Jaakko Uusitalo <jaakkouusitalo@outlook.com>
2020-08-23 13:30:16 +0300

    added jest and timeline unit test

---
+++ tests/unit/timeline.spec.js
@@ -0,0 +1,23 @@
+import { shallowMount } from '@vue/test-utils'
+import Timeline from '../../src/components/Timeline'
+
+describe('Timeline', () => {
+    it('renders start and end month', () => {
+        const wrapper = shallowMount(Timeline, {
+            propsData: {
+                started: 'Elokuu 2020',
+                ended: 'Joulukuu 2020'
+            }
+        })
+        expect(wrapper.find('p').text()).toEqual('Elokuu 2020 - Joulukuu 2020')
+    })
+    it('renders start and end month when end month is not provided', () => {
+        const wrapper = shallowMount(Timeline, {
+            propsData: {
+                started: 'Elokuu 2020',
+                ended: ''
+            }
+        })
+        expect(wrapper.find('p').text()).toEqual('Elokuu 2020')
+    })
+})
